75d415771a9d1a665fd5cacdc03667f374e4080c
Bhas1923 <136146571+Bhas1923@users.noreply.github.com>
2025-11-28 18:51:42 +0530

    Add files via upload

---
+++ LogicalOperators.java
@@ -0,0 +1,10 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean hasID = true;
+
+        System.out.println(age >= 18 && hasID);
+        System.out.println(age >= 18 || hasID);
+        System.out.println(!(age >= 18));
+    }
+}
